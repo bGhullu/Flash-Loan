3268f8b8f7b3e3d0334a3c9995433aba4cb1bfc1
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-07-09 22:50:57 +0100

    FlashLoan

---
+++ contracts/FlashLoan.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract FlashLoan {
+    IERC20 private immutable token0;
+    IERC20 private immutable token1;
+    uint24 private immutable fee;
+    IUniswapV3Pool private immutable pool;
+
+    constructor(address _token0, address _token1, uint24 _fee) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+        fee = _fee;
+        pool = IUniswapV3Pool(getPool(_token0, _token1, _fee));
+    }
+
+    function getPool(
+        address _token0,
+        address _token1,
+        uint24 _fee
+    ) internal returns (address) {}
+}
